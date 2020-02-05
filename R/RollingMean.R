e32cd8e81a89663bc92318ababb4f6d0b4253d3e
lorenzdes <59971532+lorenzdes@users.noreply.github.com>
2020-02-05 11:59:30 +0100

    Delete RollingMean.R

--- R/RollingMean.R
+++
@@ -1,6 +0,0 @@
-RollingMean <- function(z){
-  Rolling_mean <- zoo::rollapply(z,
-                                 width = 2,
-                                 FUN = mean)
-  return(Rolling_mean)
-}
