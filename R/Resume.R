414bdebd04a6d003738f937e29e7769b85f73a6c
lorenzdes <lorello97@gmail.com>
2020-02-12 12:04:07 +0100

    Delete Resume.R

--- R/Resume.R
+++
@@ -1,8 +0,0 @@
-Resume<- function(x, y){
-  Title1 <-Get_Ticker("x")
-  Title2 <- Get_Ticker("y")
-                               Stocks <- merge(x, y)
-                               Clean <- na.omit(Stocks)
-  return(Clean)
-}
-
